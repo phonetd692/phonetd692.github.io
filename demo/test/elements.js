672a24b06d415f6a7fb81df942edd150dfbf488d
phonetd692 <71721810+phonetd692@users.noreply.github.com>
2025-02-19 15:03:48 -0500

    Delete demo directory

--- demo/test/elements.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * @param {string} selector
- * @return {HTMLElement}
- */
-export const find = selector => /** @type {HTMLElement} */ (document.body.querySelector(selector))
-
-export const drawingDemo = find('y-demo-drawing')
