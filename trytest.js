d92e92114b85f6148ffaa238ccc7cf2c8fa92904
phonetd692 <71721810+phonetd692@users.noreply.github.com>
2025-03-25 07:44:52 -0400

    Create trytest.js

---
+++ trytest.js
@@ -0,0 +1,35 @@
+import {joinRoom} from './trystmin.js'
+
+const config = {appId: '3d_app_test', password: 'this is my password'}
+const room = joinRoom(config, 'yodddyne')
+console.log("Test Log");
+var selfStream = null;
+
+var button1 = document.getElementById("button1");
+button1.onclick = async function() {
+selfStream ??= await navigator.mediaDevices.getUserMedia({
+  audio: true,
+  video: false
+})
+      room.addStream(selfStream, null, "testMeta");
+    };
+    
+    var button2 = document.getElementById("button2");
+button2.onclick = function() {
+      console.log("Peers: ", room.getPeers());
+    };
+    
+    var button3 = document.getElementById("button3");
+button3.onclick = function() {
+      room.leave();
+    };
+// send stream to peers currently in the room
+
+
+// send stream to peers who join later
+room.onPeerJoin(peerId => console.log(peerId));
+
+// handle streams from other peers
+room.onPeerStream((stream, peerId, meta) => {
+	console.log("Peer stream: ", peerId, meta, stream);
+})
